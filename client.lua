f78db23b19fe7297b9b6fc6c61ba00469a6707ee
Damon <73039811+koolaash@users.noreply.github.com>
2023-07-05 03:06:24 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,6 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterNetEvent('damon:client:pdwep', function()
-    local player = GetPlayerPed(-1)
 	local ped = PlayerPedId()
     SetCurrentPedWeapon(ped, GetHashKey("WEAPON_UNARMED"), true)
 end)
