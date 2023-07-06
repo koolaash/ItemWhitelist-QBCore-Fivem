9489a95936382b5a60d96977791fc6d5dedf169e
Damon <73039811+koolaash@users.noreply.github.com>
2023-07-07 03:44:16 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -9,7 +9,7 @@ RegisterNetEvent('damon:server:pdwep', function(src, itemData)
     if not Player then return end
     for i, weapon in pairs(Config.PoliceItems) do
         if weapon == gun then
-            if Player.PlayerData.job.name == "police" then else
+            if Player.PlayerData.job.name == Config.jobName then else
                 TriggerClientEvent('damon:client:pdwep', src)
 		        TriggerClientEvent("progressbar:client:cancel", src) -- add your progressbar cancel event here
                 TriggerClientEvent('QBCore:Notify', src, 'This is Police Item!!!', 'error') -- chnage to qbcore functions if this dosent work
